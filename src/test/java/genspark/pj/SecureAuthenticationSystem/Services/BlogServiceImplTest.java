80c6e54d3030dbc0a01da6fb0cfa1e1c5b290396
Hy <hyhuynh222@gmail.com>
2024-06-20 14:30:26 -0700

    Added More Tests

--- src/test/java/genspark/pj/SecureAuthenticationSystem/Services/BlogServiceImplTest.java
+++ src/test/java/genspark/pj/SecureAuthenticationSystem/Services/BlogServiceImplTest.java
@@ -1,6 +1,7 @@
 package genspark.pj.SecureAuthenticationSystem.Services;
 
 import genspark.pj.SecureAuthenticationSystem.Entity.Blog;
+import genspark.pj.SecureAuthenticationSystem.Entity.User;
 import genspark.pj.SecureAuthenticationSystem.Repository.BlogDAO;
 import genspark.pj.SecureAuthenticationSystem.Repository.UserDAO;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,14 +18,19 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class BlogServiceImplTest {
     @Autowired
     private BlogServiceImpl blogServiceImpl;
+
+    @Autowired
+    private UserServiceImpl userService;
 
 
     @BeforeEach
@@ -40,6 +46,7 @@ public class BlogServiceImplTest {
 
         // Set the SecurityContext
         SecurityContextHolder.setContext(securityContext);
+
     }
 
     @Test
@@ -61,6 +68,41 @@ public class BlogServiceImplTest {
 
         assertEquals("testUser", result1.getAuthor());
     }
+
+    @Test
+    public void updateBlogTest(){
+        User a = new User("testUser","user");
+        userService.addUser(a);
+        Blog blog = new Blog("Valid Title1", "Valid Content", Arrays.asList("tag1", "tag2"));
+        Blog blogUpdate = new Blog();
+        blogUpdate.setTitle("UPDATED");
+
+        Blog result1 = blogServiceImpl.addBlog(blog);
+        blogUpdate.setId(result1.getId());
+
+
+        assertEquals("Valid Title1", result1.getTitle());
+        Blog result2 = blogServiceImpl.updateBlog(blogUpdate);
+        assertEquals("UPDATED", result2.getTitle());
+        assertEquals("testUser", result2.getAuthor());
+    }
+
+    @Test
+    public void updateBlogTestNoUser(){
+        Blog blog = new Blog("Valid Title1", "Valid Content", Arrays.asList("tag1", "tag2"));
+        Blog blogUpdate = new Blog();
+        blogUpdate.setTitle("UPDATED");
+
+        Blog result1 = blogServiceImpl.addBlog(blog);
+        blogUpdate.setId(result1.getId());
+
+
+        assertEquals("Valid Title1", result1.getTitle());
+        assertThrows(RuntimeException.class, () -> {
+            blogServiceImpl.updateBlog(blogUpdate);
+        });
+    }
+
 
 
 }
